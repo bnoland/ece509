fbb0135c32d839319f89ed247b84b156436b7512
Benjamin Noland <benjaminnoland93@gmail.com>
2019-04-03 17:51:25 -0400

    Classifier seems to work

--- hw5/classifier.m
+++ hw5/classifier.m
@@ -1,12 +1,12 @@
 %%
-file = "hand.jpg";
+file = "apple.jpg";
 image_data = imread(file);
 
 %%
 image(image_data);
 
 %%
-N = 2;  % Number of training patches to sample from background.
+N = 3;  % Number of training patches to sample from background.
 
 bg_patches = cell(N, 1);
 for i = 1:N
@@ -21,7 +21,47 @@ rect = ginput(2);
 Y = get_patch(image_data, rect);
 
 %%
+N = size(X, 1);
+M = size(Y, 1);
 
+gamma = 0.1;
+
+% Compute the support vector classifier.
+cvx_begin
+  variables a(5) b u(N) v(M);
+  
+  minimize norm(a) + gamma * (sum(u) + sum(v));
+  subject to
+    for i = 1:N
+      X(i,:) * a - b >= 1 - u(i);
+    end
+    for i = 1:M
+      Y(i,:) * a - b <= -(1 - v(i));
+    end
+    u >= 0;
+    v >= 0;
+cvx_end
+
+%%
+[width, height, depth] = size(image_data);
+rect = [1 1; height width];
+
+Z = get_patch(image_data, rect);
+
+class = zeros(height, width);
+
+%%
+N = size(Z, 1);
+
+% Classify each pixel in the image.
+for i = 1:N
+  obs = Z(i,:);
+  x = obs(1);
+  y = obs(2);
+  class(y, x) = get_class(obs, a, b);
+end
+
+image(class);
 
 %%
 % Extracts a patch of data from the image `image_data' defined by the
@@ -43,8 +83,18 @@ function output = get_patch(image_data, rect)
   patch = reshape(patch, 3, patch_size);
 
   output = [
+    repelem(x_coords, length(y_coords))
     repmat(y_coords, 1, length(x_coords))
-    repmat(x_coords, 1, length(y_coords))
     patch
   ]';
+end
+
+% Returns the class of an observation `obs` as computed by the support
+% vector classifier defined by `a` and `b`.
+function output = get_class(obs, a, b)
+  if obs * a - b > 0
+    output = 0;  % background
+  else
+    output = 255;  % object
+  end
 end
