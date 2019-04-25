b0f8a814876b183163a82b0bfa877a48ec8f81a8
Benjamin Noland <benjaminnoland93@gmail.com>
2019-04-25 19:49:34 -0400

    Major refactoring + got plots all set up

--- project/simulations.R
+++ project/simulations.R
@@ -33,21 +33,21 @@ signed_support <- function(beta) {
 }
 
 # Returns the linear sparsity index corresponding to size `p' of the parameter
-# vector and parameter `alpha'.
-linear_sparsity <- function(p, alpha) {
-  ceiling(alpha * p)
+# vector and parameter `gamma'.
+linear_sparsity <- function(p, gamma) {
+  ceiling(gamma * p)
 }
 
 # Returns the sublinear sparsity index corresponding to size `p' of the
-# parameter vector and parameter `alpha'.
-sublinear_sparsity <- function(p, alpha) {
-  ceiling(alpha * p / log(alpha * p))
+# parameter vector and parameter `gamma'.
+sublinear_sparsity <- function(p, gamma) {
+  ceiling(gamma * p / log(gamma * p))
 }
 
 # Returns the fractional power sparsity index corresponding to size `p' of the
-# parameter vector and parameters `alpha' and `delta'.
-fractional_power_sparsity <- function(p, alpha, delta) {
-  ceiling(alpha * p^delta)
+# parameter vector and parameters `gamma' and `delta'.
+fractional_power_sparsity <- function(p, gamma, delta) {
+  ceiling(gamma * p^delta)
 }
 
 # Returns a random `n' x `p' design matrix whose columns are generated from a
@@ -71,72 +71,108 @@ lambda_n <- function(n, p, k, sigma) {
   sqrt((2 * sigma^2 * log(k) * log(p - k)) / n)
 }
 
-p_values <- c(128, 256, 512)
-theta_seq <- seq(0.01, 2.4, length.out = 15)
-n_trials <- 20
-alpha <- 0.4
-delta <- 0.75
-sigma <- 0.5
-mu <- 0.1
-
-# Center plot titles.
-theme_update(plot.title = element_text(hjust = 0.5))
-
-success_prob_plot <- function(p, uniform, mixture = 1, sparsity_fn, ...) {
-}
-
-plot_data_list <- list()
-
-for (p in p_values) {
-  k <- fractional_power_sparsity(p, alpha, delta)
-  #k <- sublinear_sparsity(p, alpha)
-  #k <- linear_sparsity(p, alpha)
+# Returns a plot of probability of successful signed support recovery vs.
+# control parameter for the values of p given in `p_values'. `uniform' is true
+# if the random designs are to be generated using a uniform Gaussian ensemble,
+# and false for non-uniform. `alpha' is the elastic net mixing parameter.
+# `sparsity_fn' is the function defining the sparsity index, and any extra
+# parameters `...' are passed to this function.
+success_prob_plot <- function(p_values, uniform, alpha, sparsity_fn, ...) {
+  theta_seq <- seq(0.01, 2.4, length.out = 15)  # Control parameters
+  sigma <- 0.5  # Noise level
+  mu <- 0.1     # Defines the Toeplitz covariance matrix in non-uniform case
+  n_trials <- 1
   
-  S <- random_support(p, k)
-  beta_star <- true_beta(p, S)
-  S_signed <- signed_support(beta_star)
+  plot_data_list <- list()
   
-  #cov_matrix <- diag(p)
-  cov_matrix <- toeplitz(mu^(0:(p-1)))
-  
-  probs <- numeric(length(theta_seq))
-  for (i in seq_along(theta_seq)) {
-    theta <- theta_seq[[i]]
-    n <- sample_size(theta, p, k)
+  # Generate the plot data for each value of p.
+  for (p in p_values) {
+    k <- sparsity_fn(p, ...)
+    S <- random_support(p, k)
+    beta_star <- true_beta(p, S)
+    S_signed <- signed_support(beta_star)
     
-    # TODO: Should we try to avoid this from ever occurring?
-    if (n == 1) {
-      cat("n = 1 -- skipping")
-      next
-    }
+    # Select the appropriate covariance matrix for generating random designs.
+    if (uniform)
+      cov_matrix <- diag(p)
+    else
+      cov_matrix <- toeplitz(mu^(0:(p-1)))
     
-    count <- 0
-    for (j in 1:n_trials) {
-      X <- random_design(n, p, cov_matrix)
-      y <- X %*% beta_star + rnorm(n, sd = sigma)
+    # Estimate the probability of successful signed recovery support for each
+    # value of the control parameter.
+    probs <- numeric(length(theta_seq))
+    for (i in seq_along(theta_seq)) {
+      theta <- theta_seq[[i]]
+      n <- sample_size(theta, p, k)
       
-      lambda <- lambda_n(n, p, k, sigma)
-      # TODO: In order to speed up coefficient prediction, may want to
-      # precompute the sequence of regularization parameters.
-      fit <- glmnet(X, y, intercept = FALSE)
-      beta <- as.numeric(predict(fit, s = lambda, type = "coefficients",
-                                 exact = TRUE, x = X, y = y))[-1]
-      count <- count + all(signed_support(beta) == S_signed)
+      # TODO: Due to standardization, glmnet doesn't like this case. Should we
+      # try to avoid this from ever occurring?
+      if (n == 1) {
+        cat("n = 1 -- skipping")
+        next
+      }
+      
+      # Estimate the probability of successful signed support recovery by
+      # repeatedly simulating a model with random design.
+      count <- 0
+      for (j in 1:n_trials) {
+        X <- random_design(n, p, cov_matrix)
+        y <- X %*% beta_star + rnorm(n, sd = sigma)
+        
+        lambda <- lambda_n(n, p, k, sigma)
+        # TODO: In order to speed up coefficient prediction, may want to
+        # precompute the sequence of regularization parameters.
+        fit <- glmnet(X, y, intercept = FALSE, alpha = alpha)
+        beta <- as.numeric(predict(fit, s = lambda, type = "coefficients",
+                                   exact = TRUE, x = X, y = y,
+                                   alpha = alpha))[-1]
+        
+        count <- count + all(signed_support(beta) == S_signed)
+      }
+      
+      probs[[i]] <- count / n_trials
     }
     
-    probs[[i]] <- count / n_trials
+    plot_data_list[[p]] <- tibble(theta_seq, probs, p)
   }
   
-  plot_data_list[[p]] <- tibble(theta_seq, probs, p)
+  # Merge all of the plot data into one big data frame.
+  plot_data <- bind_rows(plot_data_list)
+  plot_data$p <- factor(plot_data$p, levels = p_values)
+  
+  # Generate a plot of probability of success vs. the control parameter. Display
+  # a line for each value of p.
+  plot <- ggplot(plot_data, aes(theta_seq, probs, color = p)) +
+    coord_cartesian(xlim = c(0, 2.4), ylim = c(0, 1)) +
+    geom_point() +
+    geom_line() +
+    geom_vline(xintercept = 1, linetype = "dotted") +
+    labs(color = TeX("$p$")) +
+    xlab(TeX("Control parameter $\\theta(n, p, k)$")) +
+    ylab("Probability of success") +
+    theme(aspect.ratio = 1)
+  
+  invisible(plot)
 }
 
-plot_data <- bind_rows(plot_data_list)
-plot_data$p <- factor(plot_data$p, levels = p_values)
-ggplot(plot_data, aes(theta_seq, probs, color = p)) +
-  coord_cartesian(xlim = c(0, 2.4), ylim = c(0, 1)) +
-  geom_point() +
-  geom_line() +
-  labs(color = TeX("$p$")) +
-  xlab(TeX("Control parameter $\\theta(n, p, k)$")) +
-  ylab("Probability of success") +
-  theme(aspect.ratio = 1)
+# Set global theme for plots.
+theme_set(theme_bw())
+theme_update(plot.title = element_text(hjust = 0.5))
+
+# Values of p for which to run the simulations.
+p_values <- c(128, 256, 512)
+
+# Sparsity parameters.
+gamma <- 0.4
+delta <- 0.75
+
+plot <- success_prob_plot(
+  p_values,
+  uniform = FALSE,
+  alpha = 1,
+  sparsity_fn = fractional_power_sparsity,
+  gamma,
+  delta
+)
+
+plot
